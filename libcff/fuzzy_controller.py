1e8a8c4f82f858ca2087ec82dbb2bd4550427393
Diego <diegicheri@hotmail.com>
2024-03-12 22:18:26 +1100

    Controller tunning
    Controller first tunning after testing, further adjustments are needed

--- libcff/fuzzy_controller.py
+++ libcff/fuzzy_controller.py
@@ -10,8 +10,8 @@ from fuzzylogic.classes import Domain, Set, Rule
 from fuzzylogic.hedges import very
 from fuzzylogic.functions import bounded_sigmoid, triangular_sigmoid, gauss
 from fuzzylogic.classes import rule_from_table
-#from send_setpoint import send_setpoint
-# from full_state_setpoint_demo import quaternion_from_euler
+
+data_1 = 1
 
 __author__ = 'diegoch-it'
 __all__ = ['Cffuzzyc']
@@ -35,74 +35,78 @@ class Cffuzzyc:
         self._x_vel         = Domain("Horizontal velocity", -1.0, 1.0, res = 0.01)
 
         self._zdistance     = Domain("Altitud on z axis", 0.0, 5.0, res = 0.01)
-        self._yawrate       = Domain("Yaw rate", -25, 25, res = 0.01)
+        self._yawrate       = Domain("Yaw rate", -25.0, 25.0, res = 0.01)
 
         # Input linguistic variables: Front and rear proximity sensors' membership functions
 
-        self._front_range.far   = bounded_sigmoid(1, 3)
-        self._front_range.close = gauss(1, 5, c_m = 1)#bounded_sigmoid(0.18, 0.5, inverse=True)
+        self._front_range.far   = bounded_sigmoid(2, 3)
+        self._front_range.close = bounded_sigmoid(0.18, 1, inverse=True)
 
-        self._rear_range.far    = bounded_sigmoid(1, 3)
-        self._rear_range.close  = bounded_sigmoid(0, 0.5, inverse = True)
+        self._rear_range.far    = bounded_sigmoid(2, 3)
+        self._rear_range.close  = bounded_sigmoid(0.18, 1, inverse=True)
 
         # Input linguistic variables: Right and left proximity sensors' membership functions
 
-        self._right_range.far   = bounded_sigmoid(0.2, 1.5)
-        self._right_range.close = bounded_sigmoid(0, 0.25, inverse = True)
+        self._right_range.far   = bounded_sigmoid(2, 4)
+        self._right_range.close = bounded_sigmoid(0.18, 0.5, inverse=True)
 
-        self._left_range.far    = bounded_sigmoid(0.2, 1.5)
-        self._left_range.close  = bounded_sigmoid(0, 0.25, inverse = True)
+        self._left_range.far    = bounded_sigmoid(2, 4)
+        self._left_range.close  = bounded_sigmoid(0.18, 0.5, inverse=True)
 
         # Input linguistic variables: Up and down proximity sensors' membership functions
 
         self._up_range.far      = bounded_sigmoid(0.2, 1.5)
-        self._up_range.close    = bounded_sigmoid(0, 0.25, inverse = True)
+        self._up_range.close    = bounded_sigmoid(0.0, 0.25, inverse=True)
 
         self._down_range.far    = bounded_sigmoid(0.2, 1.5)
-        self._down_range.close  = bounded_sigmoid(0, 0.25, inverse = True)
+        self._down_range.close  = bounded_sigmoid(0.0, 0.25, inverse=True)
 
         # Output linguistic variables: Membership functions
 
-        self._y_vel.forward     = bounded_sigmoid(0, 1)
-        self._y_vel.backward    = bounded_sigmoid(-2, 0, inverse = True)
+        self._y_vel.forward     = bounded_sigmoid(0.4, 1)
+        self._y_vel.backward    = bounded_sigmoid(-1.0, -0.4, inverse=True)
 
-        self._x_vel.right       = bounded_sigmoid(0.5, 1)
-        self._x_vel.left        = bounded_sigmoid(-1, -0.5, inverse=True)
+        self._x_vel.left        = bounded_sigmoid(0.5, 1)
+        self._x_vel.right       = bounded_sigmoid(-1.0, -0.5, inverse=True)
 
-        self._zdistance.up      = bounded_sigmoid(0, 0.5)
-        self._zdistance.down    = bounded_sigmoid(0, 0.5)
+        self._zdistance.up      = bounded_sigmoid(0.0, 0.5)
+        self._zdistance.down    = bounded_sigmoid(0.0, 0.5)
 
         self._yawrate.clock     = bounded_sigmoid(-0.3, 0, inverse=True )
-        self._yawrate.anticlock = bounded_sigmoid(0, 0.5)
+        self._yawrate.anticlock = bounded_sigmoid(0.0, 0.5)
 
     # Fuzzy inference rules, filters and defuzzification
-    def auto(self, rfront = 0, rback = 0, rright = 0, rleft = 0, rup = 0, rzrange = 0):
         
-        def block_sat(d, max = 3.0):
-            data_1 = 0
-            if d < max:
+    def auto(self, rfront=0, rback=0, rright=0, rleft=0, rup=0, rzrange=0):
+        
+        def block_saturation(d, max= 3.0):
+            global data_1
+            
+            if d == None:
+                return data_1
+            elif d < max:
                 data_1 = d
                 return d
             else:
                 return data_1 
         
         rules1 = Rule( {
-                        (self._front_range.far, self._rear_range.far): self._y_vel.forward,
                         (self._front_range.far, self._rear_range.close): self._y_vel.forward,
-                        (self._front_range.close, self._rear_range.far): self._y_vel.backward,
-                        (self._rear_range.close, self._rear_range.close): self._y_vel.forward
+                        (self._rear_range.close, self._rear_range.close): self._y_vel.forward,
+                        (self._front_range.close, self._front_range.close): self._y_vel.backward,
+                        (self._front_range.close, self._rear_range.far): self._y_vel.backward
                         })
         
         rules2 = Rule({
-                        (self._right_range.close, self._left_range.far): self._x_vel.left,
-                        (self._left_range.close, self._left_range.far): self._x_vel.right,
+                        (self._left_range.far, self._right_range.close): self._x_vel.left,
+                        (self._left_range.close, self._right_range.far) : self._x_vel.right,
                         })
             
         rg1 = {
-                self._front_range : block_sat(rfront), self._rear_range : block_sat(rback)
+            self._front_range : block_saturation(rfront), self._rear_range : block_saturation(rback)
             }
         rg2 = {
-                self._left_range : block_sat(rleft), self._right_range : block_sat(rright)
+            self._left_range : block_saturation(rleft), self._right_range : block_saturation(rright)
             }
         return [rules1(rg1), rules2(rg2)]
 
